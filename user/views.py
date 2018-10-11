586e4d014578d0d0c14dec0403bdae93757f2855
lidongya <342974998@qq.com>
2018-10-11 22:06:15 +0800

    登录模块

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from user.forms import RegisterForm
+from user.models import User
 from django.contrib.auth.hashers import make_password, check_password
 # Create your views here.i
 
@@ -16,7 +17,7 @@ def register(request):
             user.password = make_password(user.password)
             user.save()
 
-            return i
+            return redirect('/user/login/')
         else:
             return render(request,'register.html',{'error':form.errors})
     else:
@@ -24,18 +25,36 @@ def register(request):
 
 
 def login(request):
-
-    return render(request, 'login.html', {})
+    if request.method == 'POST':
+        nickname = request.POST.get('nickname').strip()
+        password = request.POST.get('password').strip()
+        # 检查用户是否存在
+        try:
+            user = User.objects.get(nickname=nickname)
+        except User.DoesNotExist:
+            return render(request, 'login.html', {'error':'用户不存在'})
+        # 检查密码是否正确
+        if check_password(password,user.password):
+            request.session['uid'] = user.id
+            request.session['nickname'] = user.nickname
+            request.session['avatar'] = user.icon.url
+            return redirect('/user/info/')
+        else:
+            return render(request, 'login.html', {'error':'密码错误'})
+    else:
+        return render(request, 'login.html')
 
 
 def logout(request):
-
+    request.session.flush()
     return redirect('/')
 
 
 def user_info(request):
+    uid = request.session.get('uid')
+    user = User.objects.get(pk=uid)
 
-    return render(request, 'user_info.html', {})
+    return render(request, 'user_info.html', {'user':user})
 
 
 
